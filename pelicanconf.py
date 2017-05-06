32fc9c47b7ebc9082d86235817afbd5e9bb03a71
serch <sbadillo@gmail.com>
2017-05-06 12:10:10 +0200

    updated CDN fontawesome to 4.7, added tipogrify, favicon.ico

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,10 +14,6 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'English'
 
-
-
-
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -40,8 +36,36 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# Pure theme configs:
+STATIC_PATHS = [
+    'images', 
+    'extra/favicon.ico'
+]
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
+
+# Markup
+TYPOGRIFY = True
+
+# ****************************************************************************
+# *                            Pure theme configs:                           *
+# ****************************************************************************
+
 # COVER_IMG_URL - Set the sidebar image for homepage.
 # PROFILE_IMAGE_URL - Set the image/logo for the top circle cutout on sidebar.
 TAGLINE = 'Tagline. Used for the page titles and some meta tags' # Used for the page titles and some meta tags.
+# DISQUS_ON_PAGES - Set this to True to enable disqus comments in pages.
 # DISQUS_SITENAME - Set this to enable disqus comments in articles.
+
+FAVICON = 'favicon.ico'
+
+# GOOGLE_ANALYTICS - Set the Google Analytics code (eg. "UA-000000-00")
+# PIWIK_URL and PIWIK_SITE_ID - Set the URL and site-id for Piwik tracking. (Without 'http://')
+
+# Social links syntax: (icon, link).
+# see icons here: http://fontawesome.io/cheatsheet/ (remove fa- from name)
+SOCIAL = (
+    # ('github', 'https://github.com/sbadillo/'),
+    # ('address-card', 'https://doyoubuzz.com/sergio-badillo'),
+    # ('linkedin-square', 'https://www.linkedin.com/in/sergiobadillo'),
+)
