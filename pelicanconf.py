0032aec5906694203b403e27b1aac06a6d79c2ec
serch <sbadillo@gmail.com>
2017-05-06 10:14:29 +0200

    changed theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = 'Sergio Badillo'
 SITENAME = 'SergioBadillo'
 SITEURL = ''
 
-# THEME = 'themes/pure'
+THEME = 'themes/pure'
 
 PATH = 'content'
 
@@ -39,3 +39,9 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Pure theme configs:
+# COVER_IMG_URL - Set the sidebar image for homepage.
+# PROFILE_IMAGE_URL - Set the image/logo for the top circle cutout on sidebar.
+TAGLINE = 'Tagline. Used for the page titles and some meta tags' # Used for the page titles and some meta tags.
+# DISQUS_SITENAME - Set this to enable disqus comments in articles.
