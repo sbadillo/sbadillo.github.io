0e62c3c571bf7e5dcd3ba1cc6cf4b95eac6618ab
serch <sbadillo@gmail.com>
2017-05-26 17:34:01 +0200

    draft on ML-coursera completed course

---
+++ content/completed_ML_Coursera.md
@@ -0,0 +1,5 @@
+title: Finally completed the Andrew Ng's Machine Learning course!
+date: 2017-05-26
+status: draft
+
+I started this awesome course on mid April 2017. I finished it at the end of May. It took me 1.5 months to go through all the material and solving the excercises. Final score 97.5%. I choose this course as the first introduction to machine learning and I am pretty satisfied with it as it covered much of the basics of the subject, passing throught the most widespread techniques. The excercises provided were very fun to do as well.
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Sergio Badillo'
 SITENAME = 'SergioBadillo'
-SITEURL = ''
+SITEURL = '' 
 
 THEME = 'themes/notmyidea-cms'
 # THEME = 'themes/pure'
@@ -48,26 +48,3 @@ EXTRA_PATH_METADATA = {
 
 # Markup
 TYPOGRIFY = True
-
-# ****************************************************************************
-# *                            Pure theme configs:                           *
-# ****************************************************************************
-
-# COVER_IMG_URL - Set the sidebar image for homepage.
-# PROFILE_IMAGE_URL - Set the image/logo for the top circle cutout on sidebar.
-TAGLINE = 'Tagline. Used for the page titles and some meta tags' # Used for the page titles and some meta tags.
-# DISQUS_ON_PAGES - Set this to True to enable disqus comments in pages.
-# DISQUS_SITENAME - Set this to enable disqus comments in articles.
-
-FAVICON = 'favicon.ico'
-
-# GOOGLE_ANALYTICS - Set the Google Analytics code (eg. "UA-000000-00")
-# PIWIK_URL and PIWIK_SITE_ID - Set the URL and site-id for Piwik tracking. (Without 'http://')
-
-# Social links syntax: (icon, link).
-# see icons here: http://fontawesome.io/cheatsheet/ (remove fa- from name)
-SOCIAL = (
-    # ('github', 'https://github.com/sbadillo/'),
-    # ('address-card', 'https://doyoubuzz.com/sergio-badillo'),
-    # ('linkedin-square', 'https://www.linkedin.com/in/sergiobadillo'),
-)
